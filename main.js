5a4c555e13cccfa1695d3a60df24f42842079d0f
don121316516 <a0970658231@gmail.com>
2020-06-21 16:16:00 +0800

    完成

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
+AOS.init(
+)
+
 $('#banner').slick({
     slidesToShow:1,
     slidesToScroll:1,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed:1500,
 });
 
 $('#brand').slick({
     slidesToShow:3,
     slidesToScroll:3,
+    dots: true,
+    autoplay: true,
+    autoplaySpeed:1500,
 });
